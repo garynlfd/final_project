333c725dd544ec0767baa8b76c182a1595faf571
garynlfd <garyskyking0908@gmail.com>
2018-12-28 14:16:20 +0800

    second commit

--- final_project.cpp
+++ final_project.cpp
@@ -3,7 +3,7 @@ public:
     void makeMove(int Record[5][6], int Max[5][6], Color color[5][6], Color inputColor)
     {
         x = 0; y = 0; myCM_x = 0; myCM_y = 0; enemyCM_x = 0; enemyCM_y = 0; corner_x = 0; corner_y = 0; near_x = 0; near_y = 0; empty_x = 0;
-        empty_y = 0; myCell_x = 0; myCell_y = 0; kill_x = 0; kill_y = 0; diagonal = false; danger = false; enemy_x = 0; enemy_y = 0; flag = 0;
+        empty_y = 0; myCell_x = 0; myCell_y = 0; kill_x = 0; kill_y = 0;
 
         if(inputColor == Red) enemyColor = Blue;
         else enemyColor = Red;
@@ -14,35 +14,28 @@ public:
                 x = kill_x;
                 y = kill_y;
             }
-            else if(myCM(Record, Max, color, inputColor) && cornerEmpty(Record, Max, color, inputColor))
-            {
-                x = corner_x;
-                y = corner_y;
-            }
-            else if(myCM(Record, Max, color, inputColor) && enemy_near_myCM(Record, Max, color, inputColor) && !diagonal && !danger)
-            {
-                x = kill_x;
-                y = kill_y;
-            }
-            else if(myCM(Record, Max, color, inputColor) && enemy_near_myCell(Record, Max, color, inputColor))
-            {
-                x = myCell_x;
-                y = myCell_y;
-            }
-            else if(myCM(Record, Max, color, inputColor) && myCell(Record, Max, color, inputColor))
-            {
-                x = myCell_x;
-                y = myCell_y;
-            }
-            else if(myCM(Record, Max, color, inputColor) && emptyCell(Record, Max, color, inputColor))
-            {
-                x = empty_x;
-                y = empty_y;
-            }
             else if(myCM(Record, Max, color, inputColor))
             {
-                x = myCM_x;
-                y = myCM_y;
+                if(enemy_near_myCM(Record, Max, color, inputColor))
+                {
+                    x = kill_x;
+                    y = kill_y;
+                }
+                else if(enemy_near_myCell(Record, Max, color, inputColor))
+                {
+                    x = myCell_x;
+                    y = myCell_y;
+                }
+                /*else if(myCM_near(Record, Max, color, inputColor))
+                {
+                    x = near_x;
+                    y = near_y;
+                }*/
+                else
+                {
+                    x = myCM_x;
+                    y = myCM_y;
+                }
             }
             else
             {
@@ -54,12 +47,7 @@ public:
                 }
                 else
                 {
-                    if(enemy_near_myCell(Record, Max, color, inputColor))
-                    {
-                        x = myCell_x;
-                        y = myCell_y;
-                    }
-                    else if(myCell(Record, Max, color, inputColor))
+                    if(myCell(Record, Max, color, inputColor))
                     {
                         x = myCell_x;
                         y = myCell_y;
@@ -73,35 +61,28 @@ public:
                 }
             }
         }
-        else if(myCM(Record, Max, color, inputColor) && cornerEmpty(Record, Max, color, inputColor))
-        {
-            x = corner_x;
-            y = corner_y;
-        }
-        else if(myCM(Record, Max, color, inputColor) && enemy_near_myCM(Record, Max, color, inputColor) && !diagonal && !danger)
-        {
-            x = kill_x;
-            y = kill_y;
-        }
-        else if(myCM(Record, Max, color, inputColor) && enemy_near_myCell(Record, Max, color, inputColor))
-        {
-            x = myCell_x;
-            y = myCell_y;
-        }
-        else if(myCM(Record, Max, color, inputColor) && myCell(Record, Max, color, inputColor))
-        {
-            x = myCell_x;
-            y = myCell_y;
-        }
-        else if(myCM(Record, Max, color, inputColor) && emptyCell(Record, Max, color, inputColor))
-        {
-            x = empty_x;
-            y = empty_y;
-        }
         else if(myCM(Record, Max, color, inputColor))
         {
-            x = myCM_x;
-            y = myCM_y;
+            if(enemy_near_myCM(Record, Max, color, inputColor))
+            {
+                x = kill_x;
+                y = kill_y;
+            }
+            else if(enemy_near_myCell(Record, Max, color, inputColor))
+            {
+                x = myCell_x;
+                y = myCell_y;
+            }
+            /*else if(myCM_near(Record, Max, color, inputColor))
+            {
+                x = near_x;
+                y = near_y;
+            }*/
+            else
+            {
+                x = myCM_x;
+                y = myCM_y;
+            }
         }
         else
         {
@@ -162,31 +143,8 @@ public:
                     {
                         if(color[myCM_x + dir_r_[k]][myCM_y + dir_c_[k]] == enemyColor &&
                            myCM_x + dir_r_[k] >= 0 && myCM_y + dir_c_[k] >= 0 &&
-                           myCM_x + dir_r_[k] < 5 && myCM_y + dir_c_[k] < 6 &&
-                           Max[myCM_x + dir_r_[k]][myCM_y + dir_c_[k]] - Record[myCM_x + dir_r_[k]][myCM_y + dir_c_[k]] > 1)
+                           myCM_x + dir_r_[k] < 5 && myCM_y + dir_c_[k] < 6)
                         {
-                            enemy_x = myCM_x + dir_r_[k];
-                            enemy_y = myCM_y + dir_c_[k];
-                            for(int m = 0; m < 4; ++m)
-                            {
-                                if(color[myCM_x + dir_rd_[m]][myCM_y + dir_cd_[m]] == enemyColor &&
-                                   myCM_x + dir_rd_[m] >= 0 && myCM_y + dir_cd_[m] >= 0 &&
-                                   myCM_x + dir_rd_[m] < 5 && myCM_y + dir_cd_[m] < 6 &&
-                                   Max[myCM_x + dir_rd_[m]][myCM_y + dir_cd_[m]] - Record[myCM_x + dir_rd_[m]][myCM_y + dir_cd_[m]] == 1)
-                                {
-                                    diagonal = true;
-                                    break;
-                                }
-                            }
-                            for(int m = 0; m < 4; ++m)
-                            {
-                                if(color[enemy_x + dir_r_[m]][enemy_y + dir_c_[m]] == enemyColor &&
-                                   Max[enemy_x + dir_r_[m]][enemy_y + dir_c_[m]] == Record[enemy_x + dir_r_[i]][enemy_y + dir_c_[m]] - 1)
-                                {
-                                    danger = true;
-                                    break;
-                                }
-                            }
                             kill_x = myCM_x;
                             kill_y = myCM_y;
                             return true;
@@ -237,24 +195,13 @@ public:
                         if(color[i + dir_r_[k]][j + dir_c_[k]] == enemyColor &&
                            0 <= i + dir_r_[k] && i + dir_r_[k] < 5 &&
                            0 <= j + dir_c_[k] && j + dir_c_[k] < 6 &&
-                           Max[i][j] - Record[i][j] <= Max[i + dir_r_[k]][j + dir_c_[k]] - Record[i + dir_r_[k]][j + dir_c_[k]] &&
-                           Max[i][j] - Record[i][j] > 1)
+                           Max[i][j] - Record[i][j] <= Max[i + dir_r_[k]][j + dir_c_[k]] - Record[i + dir_r_[k]][j + dir_c_[k]])
                         {
                             myCell_x = i;
                             myCell_y = j;
-                            flag = 1;
-                        }
-                        else if(color[i + dir_r_[k]][j + dir_c_[k]] == enemyColor &&
-                           0 <= i + dir_r_[k] && i + dir_r_[k] < 5 &&
-                           0 <= j + dir_c_[k] && j + dir_c_[k] < 6 ||
-                           Max[i][j] - Record[i][j] > Max[i + dir_r_[k]][j + dir_c_[k]] - Record[i + dir_r_[k]][j + dir_c_[k]] ||
-                           Max[i][j] - Record[i][j] <= 1)
-                        {
-                            flag = 0;
+                            return true;
                         }
                     }
-                    if(flag) return true;
-                    else return false;
                 }
             }
         }
@@ -329,7 +276,7 @@ public:
         {
             for(int j = 0; j < 6; ++j)
             {
-                if(color[i][j] == inputColor && Max[i][j] - Record[i][j] > 1)
+                if(color[i][j] == inputColor)
                 {
                     myCell_x = i;
                     myCell_y = j;
@@ -362,17 +309,10 @@ private:
     int myCell_y;
     int kill_x;
     int kill_y;
-    bool diagonal;
-    bool danger;
-    int enemy_x;
-    int enemy_y;
     Color enemyColor;
-    int flag;
     //bool last;
     //int last_x;
     //int last_y;
     const int dir_r_[4] = {1, -1, 0, 0};
     const int dir_c_[4] = {0, 0, 1, -1};
-    const int dir_rd_[4] = {1, -1, 1, -1};
-    const int dir_cd_[4] = {1, 1, -1, -1};
 };
